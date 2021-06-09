717eee6554edfa19396ee7fb9aa821b210890ce8
germansmolyar <german@crunchmind.com>
2021-06-10 00:53:39 +0300

    Add basic functions for adding new users

---
+++ buyer/index.js
@@ -0,0 +1,92 @@
+'use strict';
+
+const webflowAPI = require('./modules/webflow_module');
+
+
+exports.handler = async (event) => {
+    const response = {};
+    response.headers = {
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+    };
+
+    let dataFromRequest = JSON.parse(event.body);
+
+    let updateBuyerData = async () => {
+        let buyersCollectionId, sellersCollectionId, projectsCollectionId, buyersItems, sellersItems, projectsItems;
+
+        await Promise.all([webflowAPI.getCollectionIdByName('Buyers'), webflowAPI.getCollectionIdByName('Sellers'), webflowAPI.getCollectionIdByName('Projects')]).then(async (collections) => {
+            buyersCollectionId = collections[0][0]['_id'];
+            sellersCollectionId = collections[1][0]['_id'];
+            projectsCollectionId = collections[2][0]['_id'];
+            await Promise.all([webflowAPI.getCollectionItems(buyersCollectionId), webflowAPI.getCollectionItems(sellersCollectionId), webflowAPI.getCollectionItems(projectsCollectionId)]).then(async (items) => {
+                buyersItems = items[0];
+                sellersItems = items[1];
+                projectsItems = items[2];
+            });
+        });
+
+
+        let projectToAdd = projectsItems.items.filter(item => item.address.includes(dataFromRequest.Project))[0]._id;
+        let sellerToAdd = sellersItems.items.filter(item => item.slug.includes(dataFromRequest.seller))[0]._id;
+        let existingBuyer = buyersItems.items.filter(item => item['phone-number'] === dataFromRequest.Phone)[0];
+
+        if (existingBuyer) {
+            if (!existingBuyer.projects.includes(projectToAdd)) {
+                existingBuyer.projects.push(projectToAdd);
+                projectToAdd = existingBuyer.projects;
+            }
+
+            if (!existingBuyer.sellers.includes(sellerToAdd)) {
+                existingBuyer.sellers.push(sellerToAdd);
+                sellerToAdd = existingBuyer.sellers;
+            }
+
+            return await webflowAPI.updateCollectionItem(buyersCollectionId, existingBuyer['_id'], {
+                'name': dataFromRequest.name,
+                'slug': existingBuyer.slug,
+                '_archived': false,
+                '_draft': false,
+                'phone-number': dataFromRequest.Phone,
+                'projects': projectToAdd,
+                'sellers': sellerToAdd
+            });
+
+        } else {
+            return await webflowAPI.setCollectionItem(buyersCollectionId, {
+                'name': dataFromRequest.name,
+                '_archived': false,
+                '_draft': false,
+                'phone-number': dataFromRequest.Phone,
+                'projects': [projectToAdd],
+                'sellers': [sellerToAdd]
+            });
+
+        }
+    };
+
+    let updateBuyer = null;
+    let error = '';
+
+    try {
+        updateBuyer = await updateBuyerData();
+    } catch (e) {
+        error = e;
+    }
+
+    if (updateBuyer == null) {
+        response.statusCode = 400;
+        response.body = `${error.name}: ${error.msg}. ${error.problems}`;
+
+    } else {
+        response.statusCode = 200;
+        response.body = JSON.stringify(event);
+    }
+
+    console.log(response);
+    return response;
+};
+
+
+// @TODO: We need to generate link and save it in DB somehow. And error handling.
---
+++ buyer/modules/webflow_module.js
@@ -0,0 +1,43 @@
+const token = 'da89af68b3d968cd70822438d568281e39ce4dc2e6ee125ef8652c82193821d0';
+const siteId = '60ba6f9ff86d92995f70770a';
+
+const Webflow = require('webflow-api');
+const webflowClient = new Webflow({token: token});
+
+module.exports = {
+    getInfo: async () => {
+        return webflowClient.info();
+    },
+
+    getCollections: async () => {
+        return webflowClient.collections({siteId: siteId});
+    },
+
+    getCollectionIdByName: async (name) => {
+        let collections = await module.exports.getCollections();
+        return collections.filter(collection => collection.name === name);
+    },
+
+    getCollectionItems: async (collectionId) => {
+        return webflowClient.items({collectionId: collectionId}, {limit: 100});
+    },
+
+    setCollectionItem: async (collectionId, item) => {
+        let newItem = await webflowClient.createItem({
+            collectionId: collectionId,
+            fields: item
+        }, {live: true});
+        return newItem;
+    },
+    updateCollectionItem: async (collectionId, itemId, fields) => {
+        const updatedItem = await webflowClient.updateItem({
+            collectionId: collectionId,
+            itemId: itemId,
+            fields: fields
+        }, {live: true});
+        return updatedItem;
+    }
+
+};
+
+
